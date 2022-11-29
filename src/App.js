b5d0c15fd73411115fb92cd5d3908caadbeb58ac
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-11-29 23:41:49 +0600

    added a product provider higher order component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { createContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
 import routes from "./routes/routes";
 
-export const PRODUCT_CONTEXT = createContext();
-
 function App() {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch(
-            "https://raw.githubusercontent.com/mohammadtanim24h/moon-tech-reducer-context/main/products.json"
-        )
-            .then((res) => res.json())
-            .then((data) => setProducts(data));
-    }, []);
-    const value = {
-        data: products,
-    };
     return (
         <div>
-            <PRODUCT_CONTEXT.Provider value={value}>
+            <ProductProvider>
                 <RouterProvider router={routes} />
-            </PRODUCT_CONTEXT.Provider>
+            </ProductProvider>
         </div>
     );
 }
---
+++ src/context/ProductProvider.js
@@ -0,0 +1,25 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const PRODUCT_CONTEXT = createContext();
+
+const ProductProvider = ({children}) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            "https://raw.githubusercontent.com/mohammadtanim24h/moon-tech-reducer-context/main/products.json"
+        )
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
+
+    const value = {
+        data: products,
+    };
+
+    return <PRODUCT_CONTEXT.Provider value={value}>
+        {children}
+    </PRODUCT_CONTEXT.Provider>;
+};
+
+export default ProductProvider;
