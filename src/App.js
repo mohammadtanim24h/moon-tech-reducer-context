e55ad9d3d3b9c6d1695287b9c7dcb9c66edc82e2
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-11-29 23:31:42 +0600

    added context with products data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { createContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
+export const PRODUCT_CONTEXT = createContext();
+
 function App() {
-  return (
-    <div>
-      <RouterProvider router={routes} />
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch(
+            "https://raw.githubusercontent.com/mohammadtanim24h/moon-tech-reducer-context/main/products.json"
+        )
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
+    const value = {
+        data: products,
+    };
+    return (
+        <div>
+            <PRODUCT_CONTEXT.Provider value={value}>
+                <RouterProvider router={routes} />
+            </PRODUCT_CONTEXT.Provider>
+        </div>
+    );
 }
 
 export default App;
